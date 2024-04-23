8e74799668f07d8436cc22a002b99606dc519162
smartsusee <susee37432@gmail.com>
2024-04-23 15:56:42 +0530

    Hair Project

---
+++ src/component/VideoSlider.js
@@ -0,0 +1,103 @@
+import React from "react";
+// import "./css/videoslider.css";
+
+// function VideoSlider() {
+//   return (
+//     <>
+//       <div style={{ overflow: "hidden", height: "350px" }} className="bg-dark">
+//         <div class="slider-video">
+//           <div class="slider-video-aspect">
+//             <div class="slider-video-scroll">
+//               <div class="slider-video-item">
+//                 <div
+//                   class="slider-video-pic"
+//                   //   style="background-image: url('https://img.youtube.com/vi/DblEwHkde8U/mqdefault.jpg')"
+//                   style={{
+//                     backgroundImage:
+//                       "url('https://img.youtube.com/vi/DblEwHkde8U/mqdefault.jpg')",
+//                   }}
+//                 >
+//                   <div class="slider-video-caption">Batman</div>
+//                 </div>
+//                 <div class="slider-video-play">
+//                   <button class="slider-video-close">&times;</button>
+//                   <iframe
+//                     width="420"
+//                     height="315"
+//                     src="https://www.youtube.com/embed/DblEwHkde8U?enablejsapi=1&rel=0"
+//                   ></iframe>
+//                 </div>
+//               </div>
+
+//               <div class="slider-video-item">
+//                 <div
+//                   class="slider-video-pic"
+//                   style={{
+//                     backgroundImage:
+//                       "url('https://img.youtube.com/vi/zSWdZVtXT7E/mqdefault.jpg')",
+//                   }}
+//                 >
+//                   <div class="slider-video-caption">Interstellar</div>
+//                 </div>
+//                 <div class="slider-video-play">
+//                   <button class="slider-video-close">&times;</button>
+//                   <iframe
+//                     width="420"
+//                     height="315"
+//                     src="https://www.youtube.com/embed/zSWdZVtXT7E?enablejsapi=1"
+//                   ></iframe>
+//                 </div>
+//               </div>
+
+//               <div class="slider-video-item">
+//                 <div
+//                   class="slider-video-pic"
+//                   style={{
+//                     backgroundImage:
+//                       "url('https://img.youtube.com/vi/1Q8fG0TtVAY/mqdefault.jpg')",
+//                   }}
+//                 >
+//                   <div class="slider-video-caption">Wonder Women</div>
+//                 </div>
+//                 <div class="slider-video-play">
+//                   <button class="slider-video-close">&times;</button>
+//                   <iframe
+//                     width="420"
+//                     height="315"
+//                     src="https://www.youtube.com/embed/1Q8fG0TtVAY?enablejsapi=1&rel=0"
+//                   ></iframe>
+//                 </div>
+//               </div>
+
+//               <div class="slider-video-item">
+//                 <div
+//                   class="slider-video-pic"
+//                   style={{
+//                     backgroundImage:
+//                       "url('https://img.youtube.com/vi/kl8F-8tR8to/mqdefault.jpg')",
+//                   }}
+//                 >
+//                   <div class="slider-video-caption">Kingsman</div>
+//                 </div>
+//                 <div class="slider-video-play">
+//                   <button class="slider-video-close">&times;</button>
+//                   <iframe
+//                     width="420"
+//                     height="315"
+//                     src="https://www.youtube.com/embed/kl8F-8tR8to?enablejsapi=1&rel=0"
+//                   ></iframe>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//           <button class="slider-video-left"></button>
+//           <button class="slider-video-right"></button>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default VideoSlider;
+
+// export default videoslider;
